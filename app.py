1215ebe53d365c9b892fc3dd11e5e54e06b57b79
Sanzhar <yvyyvff164@gmail.com>
2026-02-22 18:46:51 +0300

    first commit

--- app.py
+++ app.py
@@ -3,8 +3,41 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def python_page():
+    return render_template("python.html")
 
+@app.route("/print")
+def print_page():
+    return render_template("print.html")
+
+@app.route("/variables")
+def variables():
+    return render_template("variables.html")
+
+@app.route("/conditions")
+def conditions():
+    return render_template("conditions.html")
+
+@app.route("/functions")
+def functions():
+    return render_template("functions.html")
+
+@app.route("/sorting")
+def sorting():
+    return render_template("sorting.html")
+
+@app.route("/arrays1")
+def arrays1():
+    return render_template("arrays1.html")
+
+@app.route("/arrays2")
+def arrays2():
+    return render_template("arrays2.html")
+
+@app.route("/add_remove")
+def add_remove():
+    return render_template("add_remove.html")
+
+# ⚠️ МАҢЫЗДЫ
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
